5e2b70c3e19c72c36d699875721195f8cc2b6fbc
Ryan <34503835+wonderballRyan@users.noreply.github.com>
2021-12-07 12:57:40 +0800

    Update test.cpp

--- 4_project/2_findContours/test.cpp
+++ 4_project/2_findContours/test.cpp
@@ -4,24 +4,23 @@
 using namespace std;
 using namespace cv;
 
-/*
 int main()
 {
     Mat moveLightDiff(Mat src, int radius);
-    Mat src = imread("F:\\img_process\\imgs\\alm.png");
-    imshow("原始图", src);
+    Mat src = imread("F:\\img_process\\imgs\\complex.png");
+    imshow("脭颅脢录脥录", src);
     Mat src_hsv, src_h, src_s, src_v, src_tophat, src_bin;
     cvtColor(src, src_hsv, COLOR_RGB2HSV);
     vector<Mat> rgb_planes;
     split(src_hsv, rgb_planes);
     src_h = rgb_planes[0];
-    imshow("H通道", src_h);
+    imshow("H脥篓碌脌", src_h);
     waitKey();
     Mat kernel = getStructuringElement(MORPH_RECT, Size(30, 30), Point(-1, -1));
     morphologyEx(src_h, src_h, MORPH_BLACKHAT, kernel, Point(-1, -1));
     imshow("BLACKPHAT", src_h);
     threshold(src_h, src_bin, 100, 255, THRESH_OTSU);
-    imshow("二值化", src_bin);
+    imshow("露镁脰碌禄炉", src_bin);
     int max = 0;
     int index = -1;
     vector<vector<Point> >contours;
@@ -34,7 +33,6 @@ int main()
         }
     }
     drawContours(src, contours, index, Scalar(0, 0, 255), 2);
-    imshow("显示轮廓", src);
+    imshow("脧脭脢戮脗脰脌陋", src);
     waitKey();
 }
-*/
